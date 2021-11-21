b7c51192f85049cb62207715ce57ff8051542b89
RobbeRDG <robbedegroeve@gmail.com>
2021-11-21 15:31:06 +0100

    Added new Grammar

---
+++ useraccess/src/main/java/be/kul/useraccess/UseraccessApplication.java
@@ -0,0 +1,13 @@
+package be.kul.useraccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseraccessApplication.class, args);
+	}
+
+}
---
+++ useraccess/src/test/java/be/kul/useraccess/UseraccessApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kul.useraccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseraccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
