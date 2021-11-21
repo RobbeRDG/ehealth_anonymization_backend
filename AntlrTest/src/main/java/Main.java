4dc85cb96f98934b0b32e5a85cef3a7f8d454c38
RobbeRDG <robbedegroeve@gmail.com>
2021-11-21 18:18:54 +0100

    Added parser files

--- AntlrTest/src/main/java/Main.java
+++ AntlrTest/src/main/java/Main.java
@@ -1,6 +1,6 @@
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+
 
 public class Main {
     public static void main(String[] args) {
